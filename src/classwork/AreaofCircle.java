7c63e316df51b04bb32a8320d35b0f1ff2986eb9
Lakshmigv <lakshmigv@yahoo.com>
2021-12-02 20:05:20 -0500

    Update AreaofCircle.java

--- src/classwork/AreaofCircle.java
+++ src/classwork/AreaofCircle.java
@@ -5,6 +5,7 @@ public class AreaofCircle {
 	public static void main(String[] args) {
 		System.out.println("In area of circle");
 		System.out.println("modified area of circle");
+		System.out.println("modified area of square and modified by another user");
 	}
 
 }
