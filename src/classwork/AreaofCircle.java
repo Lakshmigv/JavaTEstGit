741041387eec180b29523f05355dfa3a3b984d27
chakri19 <chakrisksharma@gmail.com>
2021-12-02 21:27:38 -0500

    master save

--- src/classwork/AreaofCircle.java
+++ src/classwork/AreaofCircle.java
@@ -5,10 +5,9 @@ public class AreaofCircle {
 	public static void main(String[] args) {
 		System.out.println("In area of circle");
 		System.out.println("modified area of circle");
-
 		System.out.println("Merge the area of circle");
 		System.out.println("modified area of square and modified by another user");
-
+		System.out.println("Modified in master");
 	}
 
 }
